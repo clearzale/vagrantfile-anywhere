fb95ef3c776cd48af47bc030857dac02cb60fff7
zhao.jingqing <zhao.jingqing@opg.cn>
2018-06-07 17:46:09 +0800

    更新了ubuntu-foreman的配置

---
+++ ubuntu-foreman/bootstrap.sh
@@ -0,0 +1,7 @@
+cp /vagrant/aliyun.mirror.sources.list /etc/apt/sources.list
+apt-get update
+apt-get install -y apache2
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
